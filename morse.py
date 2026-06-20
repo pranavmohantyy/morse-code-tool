b8ab83177ceac77cb5c73046c03ed5fb74ad2151
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-20 16:24:33 -0700

    add decode function

--- morse.py
+++ morse.py
@@ -2,3 +2,7 @@ morse_dict = {"A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": ".
 
 def encode(message):
     return ' '.join(morse_dict.get(char.upper(), '') for char in message)
+
+def decode(morse_code):
+    reverse_dict = {v: k for k, v in morse_dict.items()}
+    return ''.join(reverse_dict.get(code, '') for code in morse_code.split())
