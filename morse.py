0c502df69bed0df312e54584b8fa9c473e700a7a
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-21 03:44:44 -0700

    add file input and output

--- morse.py
+++ morse.py
@@ -4,31 +4,35 @@ def encode(message):
     return ' '.join(morse_dict.get(char.upper(), '') for char in message)
 
 def decode(morse_code):
-    words = morse_code.split('  ')
-    decoded = []
-    for word in words:
-        letters = word.split()
-        decoded.append(''.join(next((k for k, v in morse_dict.items() if v == letter), '') for letter in letters))
-    return ' '.join(decoded)
+    return ''.join([next(key for key, value in morse_dict.items() if value == code) for code in morse_code.split()])
 
-if __name__ == '__main__':
-    import argparse
-    import sys
+import argparse
+import sys
 
+def main():
     parser = argparse.ArgumentParser(description='Morse code encoder/decoder')
-    subparsers = parser.add_subparsers(dest='command')
-
-    encode_parser = subparsers.add_parser('encode')
-    encode_parser.add_argument('text', nargs='?', type=str)
-
-    decode_parser = subparsers.add_parser('decode')
-    decode_parser.add_argument('morse', nargs='?', type=str)
-
+    parser.add_argument('--file', '-f', help='input file with text or morse code')
+    parser.add_argument('--output', '-o', help='output file for results')
+    parser.add_argument('text', nargs='?', help='text to encode or decode')
     args = parser.parse_args()
 
-    if args.command == 'encode':
-        text = args.text or sys.stdin.read()
-        print(encode(text))
-    elif args.command == 'decode':
-        morse = args.morse or sys.stdin.read()
-        print(decode(morse))
+    if args.file:
+        with open(args.file, 'r') as f:
+            text = f.read().strip()
+    else:
+        text = args.text
+
+    if text:
+        if all(char in '.- ' for char in text):
+            result = decode(text)
+        else:
+            result = encode(text)
+
+        if args.output:
+            with open(args.output, 'w') as f:
+                f.write(result)
+        else:
+            print(result)
+
+if __name__ == '__main__':
+    main()
